ca92a0b4027253969d54f0f62257b5e087a5983f
Verma <Anushika.Verma@philips.com>
2020-09-15 13:52:32 +0530

    initial

---
+++ Receiver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.ToUpper());
+        }
+    }
+}
