fd3b4ca8fa124a5da7d14fa0445d856f07198189
Anushika Verma <anushika.verma@philips.com>
2020-09-24 00:12:41 +0530

    Update Date.cs
    resharper

--- Receiver/Date.cs
+++ Receiver/Date.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace Receiver
 {
     public class Date
     {
         #region Data Members
-        DateTime date;
+        DateTime _date;
         Hour[] hourList;
         int[] totalEveryHour;
         //int totalFootfall;
@@ -20,7 +17,10 @@ namespace Receiver
         #region Properties
         public int[] TotalEveryHour
         {
-            get { return totalEveryHour; }
+            get 
+            { 
+                return totalEveryHour; 
+            }
         }
         #endregion
 
@@ -30,16 +30,16 @@ namespace Receiver
         {
             try
             {
-                var formatStrings = new string[] { "dd-MM-yyyy", "d-MM-yyyy" };
+                var formatStrings = new [] { "dd-MM-yyyy", "d-MM-yyyy" };
                 DateTime myDate = DateTime.ParseExact(d, formatStrings, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None); 
-                date = myDate;
+                _date = myDate;
                 totalEveryHour = new int[24];
                 hourList = new Hour[24];
                 InitializeHourList();
             }
             catch (Exception e)
             {
-                Console.WriteLine("There was an error in converting data: ", e.ToString());
+                Console.WriteLine("There was an error in converting data: "+ e.ToString());
 
             }
 
@@ -58,32 +58,32 @@ namespace Receiver
         #endregion
 
         #region Methods
-        public void addNewEntry(String newEntry)
+        public void AddNewEntry(String newEntry)
         {
-            DateTime newEntryTime;
+            
             try
             {
-                var formatStrings = new string[] { "HH:mm:ss", "h:mm:ss" };
-                newEntryTime = DateTime.ParseExact(newEntry, formatStrings, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None);
+                var formatStrings = new [] { "HH:mm:ss", "h:mm:ss" };
+                DateTime newEntryTime = DateTime.ParseExact(newEntry, formatStrings, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None);
                 int h = newEntryTime.Hour;
-                hourList[h].addNewEntry(newEntry);
+                hourList[h].AddNewEntry(newEntry);
                 totalEveryHour[h] = hourList[h].TotalFootfall;
                 
             }
             catch (Exception e)
             {
-                Console.WriteLine(newEntry + "   .."+date.ToString()+"..   " + e.ToString());
+                Console.WriteLine(newEntry + "   .."+_date.ToString()+"..   " + e.ToString());
             }
             
         }
 
         public string toString()
         {
-            String s = date.ToString("dd-MM-yyyy") + " -->  "+ '\n';
+            String s = _date.ToString("dd-MM-yyyy") + " -->  "+ '\n';
             for (int i = 0; i < hourList.Length; i++)
+                foreach(var hr in hourList)
             {
-                Hour t = hourList[i];
-                s = s + t.toString()+'\n';
+                s = s + hr.toString()+'\n';
             }
             s += '\n';
             return s;
