a82ea5f5f07642978e97e5e7c2017d9ffd7a7fc3
anushika1998 <anushika.verma@philips.com>
2020-09-23 16:37:31 +0530

    complexity

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -9,6 +9,29 @@ namespace Receiver
 {
     class Program
     {
+        static void Functionalities(Database database)
+        {
+            double[] avgPerHour = database.averageFootfallPerHour();
+
+            for (int i = 0; i < avgPerHour.Length; i++)
+            {
+                Console.WriteLine(i + " Hour" + avgPerHour[i]);
+            }
+
+            Console.WriteLine("Average per hour Found");
+
+            double[] avgDailyFootfall = database.averagePerDayForWeek();
+            for (int i = 0; i < avgDailyFootfall.Length; i++)
+            {
+                Console.WriteLine(i + " Day " + avgDailyFootfall[i]);
+            }
+
+            Console.WriteLine("Average daily footfall per week Found");
+
+            int[] peakDaily = database.peakDailyFootfall();
+            Console.WriteLine("Peak daily footfall was " + peakDaily[0] + " '" + peakDaily[1] + "' days before");
+
+        }
         static void Main(string[] args)
         {
             string input;
@@ -29,26 +52,7 @@ namespace Receiver
             //Console.WriteLine(database.toString());
             Console.WriteLine("Data populated");
 
-            double[] avgPerHour = database.averageFootfallPerHour();
-            
-            for(int i = 0; i < avgPerHour.Length; i++)
-            {
-                Console.WriteLine(i + " Hour" + avgPerHour[i]);
-            }
-
-            Console.WriteLine("Average per hour Found");
-
-            double[] avgDailyFootfall = database.averagePerDayForWeek();
-            for (int i = 0; i < avgDailyFootfall.Length; i++)
-            {
-                Console.WriteLine(i + " Day " + avgDailyFootfall[i]);
-            }
-
-            Console.WriteLine("Average daily footfall per week Found");
-
-            int[] peakDaily = database.peakDailyFootfall();
-            Console.WriteLine("Peak daily footfall was " + peakDaily[0] + " '" + peakDaily[1] + "' days before");
-
+            Functionalities(database);
 
         }
     }
